72ee99f927fda84448254f57181852a394909634
Thiago C Santos <thiagocsantos.dev@gmail.com>
2024-10-24 13:13:44 -0300

    iniciando a estrutura base do component about

--- components/About.vue
+++ components/About.vue
@@ -1,7 +1,12 @@
 <template>
 
     <div class="container-about">
-        <h1>About</h1>
+           <h3>um pouco sobre nós</h3>
+        <h1>O digital é para todos</h1>
+        <div class="container-about-text">
+        <p>Na Dinâmicas Digitais, acreditamos no imenso potencial dos pequenos negócios. Sabemos que, com o suporte certo, qualquer empresa pode alcançar grandes resultados no ambiente digital. Nosso foco é entender suas necessidades e oferecer soluções personalizadas, que se adaptam à realidade do seu negócio.</p>
+        <p>Se você não tem muita familiaridade com tecnologia, fique tranquilo. Estamos aqui para guiá-lo em cada etapa, com uma linguagem próxima e suporte de verdade. Vamos transformar juntos seu negócio, sempre priorizando o que é melhor para você, com soluções que cabem no seu bolso e fazem a diferença.</p>
+        </div>
     </div>
 
 </template>
@@ -13,5 +18,42 @@
         height: 100vh;
 
     }
+
+    
+
+    h1{
+        padding: 20px;
+      
+        font-size: 3rem;
+        font-weight: 600;
+        font-family: Poppins;
+        position: relative;
+    }
+
+
+    h3{
+        margin-left: 20px;
+        margin-top: 60px;
+        font-family: 'Poppins';
+        color: gray;
+    }
+
+
+    .container-about-text{
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        flex-direction: column;
+        padding: 0px 20px 20px 20px;
+        gap: 20px;
+        font-family: Poppins;
+      
+    }
+
+
+
+
+
+
 
 </style>
